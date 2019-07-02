09d87517edc0291b633285e63929300b95189288
william <pandewilliam100@gmail.com>
2019-07-02 10:20:19 +0300

    Displaying tutor courses on mobile app

--- admin/files/functions/api/courses/home/get_home_data.php
+++ admin/files/functions/api/courses/home/get_home_data.php
@@ -6,6 +6,10 @@
         $connection = connect_database();
         $results = [];
 
+        $statement = $connection->prepare("SELECT * FROM course_categories");
+        $statement->execute();
+        $results['categories'] = $statement->fetchAll();
+
         $statement = $connection->prepare("SELECT structure_id, course_name, course_fee, course_highlight, category_id,
                                     cover_image FROM course_structure ORDER BY rating_average DESC, rating_people 
                                     DESC  LIMIT 8");
